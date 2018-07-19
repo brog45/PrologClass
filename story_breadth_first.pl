5de12f11629c566b70b701853782fad7ef21a0db
Brian Rogers <brog45@gmail.com>
2018-07-18 23:23:01 -0500

    move logging of descriptions out of action definitions

--- story_breadth_first.pl
+++ story_breadth_first.pl
@@ -33,13 +33,18 @@ log(State, Format-Values, State0) :-
     append(History, [Format-Values], History0),
     select(history(History), State, history(History0), State0).
 
+% take an action and add its description to the history
+logged_action(StateIn, StateOut) :-
+    action(StateIn, S0, Description),
+    log(S0, Description, StateOut).
+    
 print_history([]).
 print_history([Format-Values|Tail]) :-
     format(Format, Values),
     print_history(Tail).
 
 % limit story length to avoid infinite recursion
-action(State, _) :-
+action(State, _, _) :-
     member(history(History), State),
     length(History, N),
     N > 15,
@@ -47,50 +52,43 @@ action(State, _) :-
     fail.
 
 % pee
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Pee~n'-[]) :-
     member(player_in(bathroom(_)), StateIn),
     select(bladder(full), StateIn, bladder(empty), State0),
-    select(hands(_), State0, hands(dirty), State1),
-    log(State1, 'Pee~n'-[], StateOut).
+    select(hands(_), State0, hands(dirty), StateOut).
 
 % dress for work
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Dress for work~n'-[]) :-
     member(player_in(closet), StateIn),
-    select(dressed_for(bed), StateIn, dressed_for(work), State0),
-    log(State0, 'Dress for work~n'-[], StateOut).
-    
+    select(dressed_for(bed), StateIn, dressed_for(work), StateOut).
+
 % wash hands
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Wash hands~n'-[]) :-
     member(player_in(kitchen), StateIn),
-    select(hands(dirty), StateIn, hands(clean), State0),
-    log(State0, 'Wash hands~n'-[], StateOut).
+    select(hands(dirty), StateIn, hands(clean), StateOut).
 
 % eat
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Eat~n'-[]) :-
     member(player_in(kitchen), StateIn),
     member(hands(clean), StateIn),
-    select(stomach(empty), StateIn, stomach(full), State0),
-    log(State0, 'Eat~n'-[], StateOut).
+    select(stomach(empty), StateIn, stomach(full), StateOut).
 
 % grab object
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Grab ~w~n'-[Object]) :-
     member(player_in(Location), StateIn),
     \+ member(holding(_), StateIn),
-    select(object_in(Object, Location), StateIn, holding(Object), State0),
-    log(State0, 'Grab ~w~n'-[Object], StateOut).
+    select(object_in(Object, Location), StateIn, holding(Object), StateOut).
 
 % move from room to room
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Move from ~w to ~w~n'-[CurrentLocation, Location]) :-
     member(player_in(CurrentLocation), StateIn),
     connected_to(CurrentLocation, Location),
-    select(player_in(CurrentLocation), StateIn, player_in(Location), State0),
-    log(State0, 'Move from ~w to ~w~n'-[CurrentLocation, Location], StateOut).
+    select(player_in(CurrentLocation), StateIn, player_in(Location), StateOut).
 
 % drop object
-action(StateIn, StateOut) :-
+action(StateIn, StateOut, 'Drop ~w~n'-[Object]) :-
     member(player_in(Location), StateIn),
-    select(holding(Object), StateIn, object_in(Object, Location), State0),
-    log(State0, 'Drop ~w~n'-[Object], StateOut).
+    select(holding(Object), StateIn, object_in(Object, Location), StateOut).
 
 go :-
     init(State),
@@ -109,6 +107,6 @@ process_queue([HeadState|_], StateOut) :-
     !,
     log(HeadState, 'Done!~n'-[], StateOut).
 process_queue([HeadState|TailStates], StateOut) :-
-    findall(S1, action(HeadState,S1), States),
+    findall(S1, logged_action(HeadState,S1), States),
     append(TailStates, States, Queue),
     process_queue(Queue, StateOut).
