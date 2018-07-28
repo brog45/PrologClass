ec1c2207be9e3507e535501e8ef1eb213cafb73f
Brian Rogers <brog45@gmail.com>
2018-07-28 15:13:10 -0500

    copy run.pl to run_text.pl, preparing for html version

---
+++ story/run_text.pl
@@ -0,0 +1,14 @@
+:- use_module(planner).
+:- use_module(story_data).
+:- use_module(story_dcg_text).
+:- use_module(story_generator).
+
+go :-
+    init(State),
+    generate_story(State, Story),
+    phrase(story(Story), StoryCodes),
+    !,
+    string_codes(StoryString, StoryCodes),
+    writeln(StoryString).
+
+% vim: et ts=4 sw=4 ai
