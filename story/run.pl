644c9794d4242a6a90fabfb93e393986eb7506d5
Brian Rogers <brog45@gmail.com>
2018-07-28 16:41:23 -0500

    convert run.pl to an html version with an http server

--- story/run.pl
+++ story/run.pl
@@ -1,14 +1,37 @@
 :- use_module(planner).
 :- use_module(story_data).
-:- use_module(story_dcg_text).
+:- use_module(story_dcg_html).
 :- use_module(story_generator).
 
-go :-
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/html_write)).
+
+:- http_handler(/, home_page, []).
+:- http_handler('/story', story_page, []).
+
+home_page(_Request) :-
+    reply_html_page([title('Story Generator')],
+                    [   h1('Story Generator')
+                    ,   p([ a(href('/story'), 'Click here')
+                          , ' to generate a story.'
+                          ])
+                    ]
+                    ).
+
+story_page(_Request) :-
     init(State),
     generate_story(State, Story),
-    phrase(story(Story), StoryCodes),
+    phrase(story(Story), StoryHtml),
     !,
-    string_codes(StoryString, StoryCodes),
-    writeln(StoryString).
+    reply_html_page([title('A Story')], 
+                    [  h1('A Story')
+                    ,  div([class(story)], StoryHtml)
+                    ]).
+
+go :-
+    Port = 8080,
+    http_server(http_dispatch, [port(Port)]).
 
 % vim: et ts=4 sw=4 ai
