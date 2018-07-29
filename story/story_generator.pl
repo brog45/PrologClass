5a686bdfd2f72055e0e09d116bbd1c4abc2b80e2
Brian Rogers <brog45@gmail.com>
2018-07-29 14:39:52 -0500

    no need to return updated state after applying a plan step

--- story/story_generator.pl
+++ story/story_generator.pl
@@ -2,21 +2,21 @@
 :- use_module(story_data).
 :- use_module(planner).
 
-generate_story(StateIn, StoryOut) :-
-    calculate_plan(StateIn, Plan),
-    apply_plan(StateIn, Plan, _, StoryOut).
+generate_story(State, StoryOut) :-
+    calculate_plan(State, Plan),
+    apply_plan(State, Plan, StoryOut).
 
-apply_plan(State, [], State, []).
-apply_plan(StateIn, [Action|_], StateOut, [Event|TailOut]) :-
+apply_plan(_, [], []).
+apply_plan(State, [Action|_], [Event|TailOut]) :-
     choose_event(Action, Event, EventDict),
     !,
-    apply_action(StateIn, EventDict, NewState),
+    apply_action(State, EventDict, NewState),
     calculate_plan(NewState, NewPlan),
-    apply_plan(NewState, NewPlan, StateOut, TailOut).
-apply_plan(StateIn, [Action|T], StateOut, [Action|TailOut]) :-
+    apply_plan(NewState, NewPlan, TailOut).
+apply_plan(State, [Action|T], [Action|TailOut]) :-
     action(Action, ActionDict), 
-    apply_action(StateIn, ActionDict, NewState),
-    apply_plan(NewState, T, StateOut, TailOut).
+    apply_action(State, ActionDict, NewState),
+    apply_plan(NewState, T, TailOut).
 
 choose_event(Action, Event, ActionDict) :-
     random(Luck), !,
